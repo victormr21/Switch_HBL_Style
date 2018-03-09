c39dcb800598b97afc077511502f3959a71359d7
victormr21 <victormromero24@gmail.com>
2018-03-09 12:30:01 +0100

    Add files via upload

---
+++ src/resources/filelist.cpp
@@ -0,0 +1,81 @@
+/****************************************************************************
+ * Resource files.
+ * This file is generated automatically.
+ * Includes 16 files.
+ *
+ * NOTE:
+ * Any manual modification of this file will be overwriten by the generation.
+ *****************************************************************************/
+#include <resources/filelist.h>
+
+extern const u8 bgMusic_ogg[];
+extern const u32 bgMusic_ogg_size;
+
+extern const u8 button_png[];
+extern const u32 button_png_size;
+
+extern const u8 buttonA_png[];
+extern const u32 buttonA_png_size;
+
+extern const u8 button_click_mp3[];
+extern const u32 button_click_mp3_size;
+
+extern const u8 font_ttf[];
+extern const u32 font_ttf_size;
+
+extern const u8 hbmenulogo_png[];
+extern const u32 hbmenulogo_png_size;
+
+extern const u8 homebrewButton_png[];
+extern const u32 homebrewButton_png_size;
+
+extern const u8 launchMenuBox_png[];
+extern const u32 launchMenuBox_png_size;
+
+extern const u8 leftArrow_png[];
+extern const u32 leftArrow_png_size;
+
+extern const u8 player1_point_png[];
+extern const u32 player1_point_png_size;
+
+extern const u8 player2_point_png[];
+extern const u32 player2_point_png_size;
+
+extern const u8 player3_point_png[];
+extern const u32 player3_point_png_size;
+
+extern const u8 player4_point_png[];
+extern const u32 player4_point_png_size;
+
+extern const u8 progressWindow_png[];
+extern const u32 progressWindow_png_size;
+
+extern const u8 rightArrow_png[];
+extern const u32 rightArrow_png_size;
+
+extern const u8 wave_png[];
+extern const u32 wave_png_size;
+
+static ResourceFile ResourceList[] =
+{
+	{"bgMusic.ogg", bgMusic_ogg, bgMusic_ogg_size, NULL, 0},
+	{"button.png", button_png, button_png_size, NULL, 0},
+	{"buttonA.png", buttonA_png, buttonA_png_size, NULL, 0},
+	{"button_click.mp3", button_click_mp3, button_click_mp3_size, NULL, 0},
+	{"font.ttf", font_ttf, font_ttf_size, NULL, 0},
+	{"hbmenulogo.png", hbmenulogo_png, hbmenulogo_png_size, NULL, 0},
+	{"homebrewButton.png", homebrewButton_png, homebrewButton_png_size, NULL, 0},
+	{"launchMenuBox.png", launchMenuBox_png, launchMenuBox_png_size, NULL, 0},
+	{"leftArrow.png", leftArrow_png, leftArrow_png_size, NULL, 0},
+	{"player1_point.png", player1_point_png, player1_point_png_size, NULL, 0},
+	{"player2_point.png", player2_point_png, player2_point_png_size, NULL, 0},
+	{"player3_point.png", player3_point_png, player3_point_png_size, NULL, 0},
+	{"player4_point.png", player4_point_png, player4_point_png_size, NULL, 0},
+	{"progressWindow.png", progressWindow_png, progressWindow_png_size, NULL, 0},
+	{"rightArrow.png", rightArrow_png, rightArrow_png_size, NULL, 0},
+	{"wave.png", wave_png, wave_png_size, NULL, 0},
+	{NULL, NULL, 0, NULL, 0}
+};
+
+ResourceFile * getResourceList(){ return ResourceList; }
+
